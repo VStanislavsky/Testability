7a726dd36471681b6d501e631863e994f991b351
VStanislavsky <stanislavvlasov@mail.ru>
2022-01-11 21:11:26 +0300

    Initinal commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,14 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+
+        int oneMile = 20;
+        int totalMiles = price/oneMile;
+
+        return totalMiles;
+
+    }
+
+
+}
+
